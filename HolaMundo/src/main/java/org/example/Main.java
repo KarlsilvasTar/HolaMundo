f4dd5351d4b7ddb048b130c0f9374470a73f5fe7
KarlsilvasTar <karoltatianasilvajimenez@gmail.com>
2024-11-08 13:14:18 -0500

    Crear proyecto
    Se crea el proyecto en java y se deja listo para los cambios.

---
+++ HolaMundo/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+                JOptionPane.showMessageDialog(null, "¡Hola Mundo! desde el local");
+            }
+        }
+
+
